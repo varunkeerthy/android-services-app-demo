e4786c551cb2ee3012c393d8958ab0abb0f428a8
Varun Keerthy <vkeerthy@btes.com.au>
2018-07-24 06:32:22 +1000

    Feature: Custom Android service

--- app/src/main/java/tutorial/demo/btes/com/servicesapp/MyService.java
+++ app/src/main/java/tutorial/demo/btes/com/servicesapp/MyService.java
@@ -1,7 +1,12 @@
 package tutorial.demo.btes.com.servicesapp;
 
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.app.TaskStackBuilder;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
@@ -30,6 +35,21 @@ public class MyService extends IntentService {
 
     private void showText(final String text) {
         Log.v("MyService", text);
+        Intent intent = new Intent(this, MainActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(intent);
+        PendingIntent pendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification notification = new Notification.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentText("Helllo")
+                .setAutoCancel(true)
+                .setPriority(Notification.PRIORITY_MAX)
+                .setContentIntent(pendingIntent)
+                .setContentText(text)
+                .build();
+        NotificationManager notificationManager =  (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(NOTIFICATION_ID,notification);
 
     }
 
